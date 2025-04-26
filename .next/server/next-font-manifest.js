756876ed2eb7e72e28bdb8363c2985058cd050ea
Keiji Miyake <k.miyake01+dev@gmail.com>
2025-04-26 14:39:17 +0900

    fix: .next ignore

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/home/dev/workspace/github.com/Keiji-Miyake/120-web-projects/src/app/layout\":[\"static/media/569ce4b8f30dc480-s.p.woff2\",\"static/media/93f479601ee12b01-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
